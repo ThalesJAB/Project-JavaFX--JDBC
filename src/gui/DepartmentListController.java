fbba37f0ba8c5f4813628f4c9a3ffcc68889ba6a
Thales José Aguiar Batista <thalesjoseaguiar@gmail.com>
2021-06-15 14:17:18 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
 import javafx.collections.FXCollections;
@@ -26,9 +27,10 @@ import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
-public class DepartmentListController implements Initializable {
+public class DepartmentListController implements Initializable, DataChangeListener {
 
 	private DepartmentService service;
+	
 	@FXML
 	private TableView<Department> tableViewDepartment;
 
@@ -90,6 +92,8 @@ public class DepartmentListController implements Initializable {
 			controller.setDepartment(obj);
 			controller.setDepartmentService(new DepartmentService());
 			controller.updateFormData();
+			controller.subscribeDataChangeListener(this);
+			
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Enter department data");
 			dialogStage.setScene(new Scene(pane));
@@ -102,6 +106,12 @@ public class DepartmentListController implements Initializable {
 		}catch(IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
+	}
+
+	@Override
+	public void onDataChanged() {
+		updateTableView();
+		
 	}
 
 }
